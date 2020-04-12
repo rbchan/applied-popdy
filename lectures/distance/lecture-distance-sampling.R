9d47f02942fcff47d06920d85f21fd2637aeeb6a
Richard Chandler <richard.chandlers@gmail.com>
2020-04-12 12:57:03 -0400

    Distance sampling lab

---
+++ lectures/distance/lecture-distance-sampling.R
@@ -0,0 +1,5 @@
+## ----eval=FALSE,include=FALSE--------------------------------------------
+## source("../../rnw2pdf.R")
+## rnw2pdf("lecture-distance-sampling")
+## rnw2pdf("lecture-distance-sampling",tangle=TRUE)
+
