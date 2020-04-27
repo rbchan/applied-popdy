b1ad3b6ebd525bc2cc41bf7cbe235d50530bc63b
Richard Chandler <richard.chandlers@gmail.com>
2020-04-27 11:14:58 -0400

    Mark-recap open pops

---
+++ lectures/cap-recapII/lecture-mark-recap-openpop.R
@@ -0,0 +1,5 @@
+## ----eval=FALSE,include=FALSE--------------------------------------------
+## source("../../rnw2pdf.R")
+## rnw2pdf("lecture-mark-recap-openpop")
+## rnw2pdf("lecture-mark-recap-openpop",tangle=TRUE)
+
---
+++ lectures/survival/lecture-survival.R
@@ -0,0 +1,5 @@
+## ----eval=FALSE,include=FALSE--------------------------------------------
+## source("../../rnw2pdf.R")
+## rnw2pdf("lecture-survival")
+## rnw2pdf("lecture-survival",tangle=TRUE)
+
