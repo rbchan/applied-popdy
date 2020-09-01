af6bba9969cdf8ecb38f8e8ff53f0c09b202c95a
rbchan <richard.chandlers@gmail.com>
2020-09-01 16:32:08 -0400

    logistic growth R script

--- lectures/logistic-growth/lecture-logistic-growth.R
+++ lectures/logistic-growth/lecture-logistic-growth.R
@@ -1,10 +1,10 @@
-## ----eval=FALSE,include=FALSE--------------------------------------------
+## ----eval=FALSE,include=FALSE-------------------------------------------------
 ## source("../../rnw2pdf.R")
 ## rnw2pdf("lecture-logistic-growth")
 ## rnw2pdf("lecture-logistic-growth",tangle=TRUE)
 
 
-## ----geomVlogistic,echo=FALSE--------------------------------------------
+## ----geomVlogistic,echo=FALSE-------------------------------------------------
 #Year <- 2000:3000
 Time <- 0:100
 T <- length(Time)
@@ -17,13 +17,13 @@ for(t in 2:T) {
     Nl[t] <- Nl[t-1] + Nl[t-1]*rmax*(1 - Nl[t-1]/K)
 }
 
-## ----geom,include=FALSE,echo=FALSE---------------------------------------
+## ----geom,include=FALSE,echo=FALSE--------------------------------------------
 plot(Time, Ng, lwd=5, type="l", ylim=c(0, 100), cex.lab=1.5,
      xlab="Time (t)", ylab="Population size (N)")
 legend(50, 100, c("Geometric growth", ""), lty=1, col=c("black", NA), lwd=4)
 
 
-## ----geom-logistic,include=FALSE,echo=FALSE------------------------------
+## ----geom-logistic,include=FALSE,echo=FALSE-----------------------------------
 plot(Time, Ng, lwd=5, type="l", ylim=c(0, 100), cex.lab=1.5,
      xlab="Time (t)", ylab="Population size (N)")
 lines(Time, Nl, lwd=4, col="purple")
@@ -31,7 +31,7 @@ legend(50, 100, c("Geometric growth", "Logistic growth"), lty=1,
        col=c("black","purple"), lwd=4)
 
 
-## ----lr1,include=FALSE,echo=FALSE,fig.width=12,fig.height=4--------------
+## ----lr1,include=FALSE,echo=FALSE,fig.width=12,fig.height=4-------------------
 par(mai=c(0.9,1,0.3,0.3))
 plot(Time, Nl, lwd=4, type="l", ylim=c(0, 60), cex.lab=1.9,
      xlab="Time (t)", ylab="Population size (N)", col="purple")
@@ -40,7 +40,7 @@ points(Time[which.min(abs(Nl-K/2))], Nl[which.min(abs(Nl-K/2))],
 text(Time[which.min(abs(Nl-K/2))]+5, Nl[which.min(abs(Nl-K/2))],
      labels="Inflection point (K/2)", pos=4, cex=1.5)
 
-## ----lambda,include=FALSE,echo=FALSE,fig.width=12,fig.height=4-----------
+## ----lambda,include=FALSE,echo=FALSE,fig.width=12,fig.height=4----------------
 lambda <- Nl[-1]/Nl[-T]
 par(mai=c(0.9,1,0.3,0.3))
 plot(Time[-1], lambda, lwd=4, type="l", cex.lab=1.9,
@@ -48,7 +48,7 @@ plot(Time[-1], lambda, lwd=4, type="l", cex.lab=1.9,
      ylab=expression(paste("Growth rate: ", "(", lambda, "=", N[t+1]/N[t], ")")))
 
 
-## ----lr2,include=FALSE,echo=FALSE,fig.width=12,fig.height=4--------------
+## ----lr2,include=FALSE,echo=FALSE,fig.width=12,fig.height=4-------------------
 par(mai=c(0.9,1,0.3,0.3))
 plot(Time, Nl, lwd=4, type="l", ylim=c(0, 60), cex.lab=1.9,
      xlab="Time (t)", ylab="Population size (N)", col="purple")
@@ -57,7 +57,7 @@ points(Time[which.min(abs(Nl-K/2))], Nl[which.min(abs(Nl-K/2))],
 text(Time[which.min(abs(Nl-K/2))]+5, Nl[which.min(abs(Nl-K/2))],
      labels="Inflection point (K/2)", pos=4, cex=1.5)
 
-## ----delta,include=FALSE,echo=FALSE,fig.width=12,fig.height=4------------
+## ----delta,include=FALSE,echo=FALSE,fig.width=12,fig.height=4-----------------
 delta <- Nl[-1] - Nl[-T]
 par(mai=c(0.9,1,0.3,0.3))
 plot(Time[-1], delta, lwd=4, type="l", cex.lab=1.9,
@@ -65,7 +65,7 @@ plot(Time[-1], delta, lwd=4, type="l", cex.lab=1.9,
      ylab=expression(paste("Growth: ", "(", Delta, "N =", N[t+1]-N[t], ")")))
 
 
-## ----delta-N,include=FALSE,echo=FALSE,fig.width=8,fig.height=6-----------
+## ----delta-N,include=FALSE,echo=FALSE,fig.width=8,fig.height=6----------------
 plot(Nl[-T], delta, lwd=4, type="l", cex.lab=1.2,
      xlab="Population size (N)",
      ylab=expression(paste("Growth: ", "(", Delta, "N =", N[t+1]-N[t], ")")))
@@ -74,7 +74,7 @@ abline(v=K/2, lty=3, lwd=2)
 text(K/2, 0.05, "Inflection point (K/2)", pos=4)
 
 
-## ----echo=FALSE----------------------------------------------------------
+## ----echo=FALSE---------------------------------------------------------------
 Nl4 <- Nl3 <- Nl2 <- Nl
 Nl4[1] <- Nl3[1] <- Nl2[1] <- 2
 for(t in 2:T) {
@@ -83,20 +83,20 @@ for(t in 2:T) {
     Nl4[t] <- Nl4[t-1] + Nl4[t-1]*3.0*(1-Nl4[t-1]/K)
 }
 
-## ----Nl,include=FALSE,echo=FALSE,fig.width=8,fig.height=6----------------
+## ----Nl,include=FALSE,echo=FALSE,fig.width=8,fig.height=6---------------------
 plot(Time, Nl, lwd=4, type="l", ylim=c(0, 100), cex.lab=1.3,
      xlab="Time (t)", ylab="Population size (N)", col="purple")
 legend(0, 100, c("rmax=0.1", "", "", ""),
        lwd=4, col=c("purple", NA, NA, NA))
 
-## ----Nl2,include=FALSE,echo=FALSE,fig.width=8,fig.height=6---------------
+## ----Nl2,include=FALSE,echo=FALSE,fig.width=8,fig.height=6--------------------
 plot(Time, Nl, lwd=4, type="l", ylim=c(0, 100), cex.lab=1.3,
      xlab="Time (t)", ylab="Population size (N)", col="purple")
 lines(Time, Nl2, lwd=4, col="blue")
 legend(0, 100, c("rmax=0.1", "rmax=0.5", "", ""),
        lwd=4, col=c("purple", "blue", NA, NA))
 
-## ----Nl3,include=FALSE,echo=FALSE,fig.width=8,fig.height=6---------------
+## ----Nl3,include=FALSE,echo=FALSE,fig.width=8,fig.height=6--------------------
 plot(Time, Nl, lwd=4, type="l", ylim=c(0, 100), cex.lab=1.3,
      xlab="Time (t)", ylab="Population size (N)", col="purple",
      main="Damped oscillation")
@@ -105,7 +105,7 @@ lines(Time, Nl3, lwd=4, col="orange")
 legend(0, 100, c("rmax=0.1", "rmax=0.5", "rmax=2.0", ""),
        lwd=4, col=c("purple", "blue", "orange", NA))
 
-## ----Nl4,include=FALSE,echo=FALSE,fig.width=8,fig.height=6---------------
+## ----Nl4,include=FALSE,echo=FALSE,fig.width=8,fig.height=6--------------------
 plot(Time, Nl, lwd=4, type="l", ylim=c(0, 100), cex.lab=1.3,
      xlab="Time (t)", ylab="Population size (N)", col="purple",
      main="Chaos")
@@ -116,7 +116,7 @@ legend(0, 100, c("rmax=0.1", "rmax=0.5", "rmax=2.0", "rmax=3.0"),
        lwd=4, col=c("purple", "blue", "orange", "gray"))
 
 
-## ----Nl5,include=FALSE,echo=FALSE,fig.width=8,fig.height=6---------------
+## ----Nl5,include=FALSE,echo=FALSE,fig.width=8,fig.height=6--------------------
 Nl5 <- Nl
 Nl5[1] <- 2
 Nl5[2] <- 3
@@ -130,7 +130,7 @@ plot(Nl5, type="l", xlab="Time (t)", ylab="Population size (N)",
      lwd=4, main="r=1.1, K=50, lag=2")
 
 
-## ----Nl6,include=FALSE,echo=FALSE,fig.width=8,fig.height=6---------------
+## ----Nl6,include=FALSE,echo=FALSE,fig.width=8,fig.height=6--------------------
 Nl6 <- Nl
 Nl6[1] <- 50
 k0 <- K
@@ -145,7 +145,7 @@ plot(Nl6, type="l", xlab="Time (t)", ylab="Population size (N)",
      cex.lab=1.3, panel.first=abline(h=K, col="blue", lty=2),
      lwd=4, main="r=1.1, k0=50, k1=10, c=20")
 
-## ----Nl6-2,include=FALSE,echo=FALSE,fig.width=8,fig.height=6-------------
+## ----Nl6-2,include=FALSE,echo=FALSE,fig.width=8,fig.height=6------------------
 plot(Nl6, type="l", xlab="Time (t)", ylab="Population size (N)",
      cex.lab=1.3, panel.first=abline(h=K, col="blue", lty=2),
      lwd=4, col=1, main="k0=50, k1=10, c=20", xlim=c(25, 55),
