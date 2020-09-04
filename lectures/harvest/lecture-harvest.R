3b5b7022b2c6edaa85dd96df8d53e5900f378f84
rbchan <richard.chandlers@gmail.com>
2020-09-04 13:31:59 -0400

    harvest lab and lecture

--- lectures/harvest/lecture-harvest.R
+++ lectures/harvest/lecture-harvest.R
@@ -1,10 +1,10 @@
-## ----eval=FALSE,include=FALSE--------------------------------------------
+## ----eval=FALSE,include=FALSE-------------------------------------------------
 ## source("../../rnw2pdf.R")
 ## rnw2pdf("lecture-harvest")
 ## rnw2pdf("lecture-harvest",tangle=TRUE)
 
 
-## ----msy1,include=FALSE,echo=FALSE---------------------------------------
+## ----msy1,include=FALSE,echo=FALSE--------------------------------------------
 N <- seq(0, 1000, by=10)
 rmax <- 0.1
 K <- 1000
@@ -20,7 +20,7 @@ abline(h=0, col=gray(0.7))
 segments(K/2, 0, K/2, max(H.1), lty=2, lwd=2, col="purple")
 
 
-## ----msy2,include=FALSE,echo=FALSE---------------------------------------
+## ----msy2,include=FALSE,echo=FALSE--------------------------------------------
 N <- seq(0, 1000, by=10)
 rmax <- 0.5
 K <- 1000
@@ -34,14 +34,14 @@ abline(h=0, col=gray(0.7))
 segments(K/2, 0, K/2, max(H), lty=2, lwd=2, col="purple")
 
 
-## ----phi1,echo=FALSE,include=FALSE,fig.width=8,fig.height=6--------------
+## ----phi1,echo=FALSE,include=FALSE,fig.width=8,fig.height=6-------------------
 plot(function(x) 0.8 - 0.005*x, 0, 100, col="blue",
      xlab="Population size (N)",
      ylab="Survival (S)",
      ylim=c(0, 1), lwd=2)
 
 
-## ----phibar,include=FALSE,echo=FALSE,fig.height=6,fig.width=8------------
+## ----phibar,include=FALSE,echo=FALSE,fig.height=6,fig.width=8-----------------
 plot(function(h) (1-h)*(0.8 - 0.005*(100 - 100*h)), 0, 1,
      ylim=c(0,0.6), xlab="Harvest rate (h)",
      ylab="Overall survival rate",
