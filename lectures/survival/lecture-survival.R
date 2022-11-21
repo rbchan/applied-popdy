774d9dfbb86597c9aa24fe37f699fd431027821f
rbchan <richard.chandlers@gmail.com>
2022-11-21 10:01:45 -0500

    survival

--- lectures/survival/lecture-survival.R
+++ lectures/survival/lecture-survival.R
@@ -1,4 +1,4 @@
-## ----eval=FALSE,include=FALSE--------------------------------------------
+## ----eval=FALSE,include=FALSE-------------------------------------------------
 ## source("../../rnw2pdf.R")
 ## rnw2pdf("lecture-survival")
 ## rnw2pdf("lecture-survival",tangle=TRUE)
